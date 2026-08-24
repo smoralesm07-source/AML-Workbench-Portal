53957afe898004f85514e160d732dd6fcfa588db
smoralesm07-source <smoralesm07@gmail.com>
2026-08-24 10:17:19 -0400

    config(osint): add public Sherlock connector config

---
+++ assets/atlas-osint-config.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/* Configuración pública del conector OSINT.
+ * No contiene secretos. `apiBase` debe apuntar al microservicio
+ * services/atlas-osint-sherlock desplegado detrás de HTTPS.
+ */
+window.__ATLAS_OSINT_CONFIG__={
+  enabled:true,
+  apiBase:''
+};
